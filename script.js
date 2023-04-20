eb70eb43984b60a2e0270d07b6ed2e5949fed3de
Thyago Lobato <thyagollobato@gmail.com>
2023-04-19 21:58:38 -0300

    Aprofundando em promises

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if (r.erro) {
+        throw Error('Esse cep não existe!')
+    } else 
+    console.log(r)})
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluído!'));
 
 console.log(consultaCep);
